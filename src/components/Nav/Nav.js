071625aa521ef42fc4018a5265de4786b847e608
Damian <wilczynskid@o2.pl>
2020-10-21 17:52:02 +0200

    archiwum

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -10,29 +10,30 @@ const Nav = props => {
 
     // open & close mobile menu
     const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false)
-    let styleMobileMenuList = isOpenMobileMenu ? style.listOpen : '' //menu list close/open
+    let styleMobileMenu = isOpenMobileMenu ? style.listOpen : '' //menu list close/open
+    let styleMobileButtonBurger = isOpenMobileMenu ? style.burgerOpen : '' //button burger close/open
 
 
 
     return (
-        <nav className={style.background}>
-            <div className={style.container}>
+        <header className={style.background}>
+            <nav className={style.container}>
                 <div className={style.header}>
                     <img className={style.headerImg} src={logo} alt='logo' />
                     <p className={style.headerDesc}>studio-www.com</p>
                 </div>
-                <ul onClick={() => setIsOpenMobileMenu(false)} className={`${style.list} ${styleMobileMenuList}`}>
+                <ul onClick={() => setIsOpenMobileMenu(false)} className={`${style.list} ${styleMobileMenu}`}>
                     <li className={style.listItem}><NavLink to='/home' activeClassName={style.activeLink} className={style.listItemAnchor}>STRONA GŁÓWNA</NavLink></li>
                     <li className={style.listItem}><NavLink to='/offer' activeClassName={style.activeLink} className={style.listItemAnchor}>OFERTA</NavLink></li>
                     <li className={style.listItem}><NavLink to='/login' activeClassName={style.activeLink} className={style.listItemAnchor}>STREFA KLIENTA</NavLink></li>
                     <li className={style.listItem}><NavLink to='/contact' activeClassName={style.activeLink} className={style.listItemAnchor}>KONTAKT</NavLink></li>
                 </ul>
-                <div onClick={() => setIsOpenMobileMenu(!isOpenMobileMenu)} className={style.burgerMenu}>
+                <div onClick={() => setIsOpenMobileMenu(!isOpenMobileMenu)} className={`${style.burgerMenu} ${styleMobileButtonBurger}`}>
                     <div className={style.burgerBtn}></div>
                 </div>
 
-            </div>
-        </nav>
+            </nav>
+        </header>
     )
 }
 
--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -5,14 +5,23 @@ import style from './Offer.module.css'
 // assets
 import intro from '../../assets/intro_blue_cubes.mp4'
 import not_ready from '../../assets/not_ready.jpg'
+import wolfmobileapps from '../../assets/wolfmobileapps.png'
 import food from '../../assets/food.png'
 import next from '../../assets/next.png'
 import careo from '../../assets/careo.png'
 import health from '../../assets/health.png'
 import crypto from '../../assets/crypto.png'
 import design from '../../assets/design.png'
-import gym from '../../assets/gym.png'
 import mobile from '../../assets/mobile.png'
+
+// svg
+import { ReactComponent as Card } from '../../assets/card.svg'
+import { ReactComponent as StandOut } from '../../assets/stand_out.svg'
+import { ReactComponent as Devices } from '../../assets/devices.svg'
+import { ReactComponent as Domain } from '../../assets/domain.svg'
+import { ReactComponent as Wallet } from '../../assets/wallet.svg'
+import { ReactComponent as Preferences } from '../../assets/preferences.svg'
+import { ReactComponent as Mobile } from '../../assets/mobile.svg'
 
 
 const Offer = () => {
@@ -23,10 +32,15 @@ const Offer = () => {
         <div className={style.background}>
             <div className={style.container}>
 
+                {/* movie */}
+                <div className={style.video_container}>
+                    <video className={style.video} src={intro} autoPlay muted controls alt='intro strony internetowe i aplikacje webowe' />
+                </div>
+
                 {/* section Tworzenie stron www */}
                 <section className={style.pages}>
 
-                    <div data-aos="flip-up" className={style.pages_content}>
+                    <div className={style.pages_content}>
                         <div className={style.pages_anim_container}>
                             <div className={style.pages_anim_background}>
                                 <h1 className={style.pages_anim_header}>Strony internetowe</h1>
@@ -35,12 +49,49 @@ const Offer = () => {
                                 <h2 className={style.pages_anim_header}>Strony internetowe</h2>
                             </div>
                         </div>
-                        <p className={style.pages_desc}>Strona internetowa to wizytówka Twojej marki w Internecie, która nie tylko pozwala uzyskać podstawowe informacje o firmie, ale jest również wyjątkową okazją do wyróżnienia się na tle konkurencji i zachęcenia klientów do zakupu Twoich produktów lub usług.</p>
-                        <p className={style.pages_desc}>Jeśli nie wiesz czym jest: domena, hosting, dns itp. to nic nie szkodzi. Przedstaw swój pomysł a ja zajmę się resztą. Zapytaj o ofertę - to nic nie kosztuje</p>
+                    </div>
+
+                    <div className={style.pages_desc}>
+                        <div className={style.pages_desc_content}>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Strona internetowa to wizytówka Twojej marki w Internecie, która pozwala uzyskać informacje o firmie.</p>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <Card />
+                            </div>
+                        </div>
+
+                        <div data-aos="customAnimation" className={style.pages_desc_content}>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <StandOut />
+                            </div>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Własna strona internetowa jest wyjątkową okazją do wyróżnienia się na tle konkurencji i zachęcenia klientów do zakupu Twoich produktów lub usług.</p>
+                        </div>
+
+                        <div className={style.pages_desc_content}>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Dzięki projektowaniu RWD (Responsive Web Design) Twoja strona wygląda dobrze na każdym urządzeniu niezależnie czy kożystasz z dużego monitora, smartfonu lub tableta.</p>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <Devices />
+                            </div>
+                        </div>
+
+                        <div className={style.pages_desc_content}>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <Domain />
+                            </div>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Jeśli nie wiesz czym jest: domena, hosting, dns itp. to nic nie szkodzi. Przedstaw swój pomysł a ja zajmę się resztą.</p>
+                        </div>
+
+                        <div className={style.pages_desc_content}>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Zapytaj o ofertę - to nic nie kosztuje.</p>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <Wallet />
+                            </div>
+                        </div>
                     </div>
 
                     <h1 className={style.pages_h2}>Obejrzyj przykładowe srony www:</h1>
                     <div className={style.pages_list}>
+                        <a data-aos="zoom-in" className={style.pages_a} href='https://wolfmobileapps.com/pl/' target='blank'><img className={style.pages_img} src={wolfmobileapps} alt='strona internetowa wolfmobileapps' />
+                        </a>
                         <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/food' target='blank'><img className={style.pages_img} src={food} alt='strona internetowa food' />
                         </a>
                         <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/next' target='blank'><img className={style.pages_img} src={next} alt='strona internetowa next' />
@@ -51,25 +102,19 @@ const Offer = () => {
                         </a>
                         <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/crypto' target='blank'><img className={style.pages_img} src={crypto} alt='strona internetowa crypto' />
                         </a>
-                        <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/gym' target='blank'><img className={style.pages_img} src={gym} alt='strona internetowa gym' />
-                        </a>
                         <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/design' target='blank'><img className={style.pages_img} src={design} alt='strona internetowa design' />
                         </a>
                         <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/careo' target='blank'><img className={style.pages_img} src={careo} alt='strona internetowa careo' />
                         </a>
+                        {/* <a data-aos="zoom-in" className={style.pages_a} href='https://studio-exapmle-projects.web.app/gym' target='blank'><img className={style.pages_img} src={gym} alt='strona internetowa gym' />
+                        </a> */}
                     </div>
                 </section>
 
-                <hr />
-                {/* movie */}
-
-                <video className={style.video} src={intro} autoPlay muted controls alt='intro strony internetowe i aplikacje webowe' />
-
-                <hr />
 
                 {/* section Tworzenie aplikacji webowych */}
                 <section id='web' className={style.pages}>
-                    <div data-aos="flip-up" className={style.pages_content}>
+                    <div className={style.pages_content}>
                         <div className={style.pages_anim_container}>
                             <div className={style.pages_anim_background}>
                                 <h1 className={style.pages_anim_header}>Aplikacje webowe</h1>
@@ -78,8 +123,21 @@ const Offer = () => {
                                 <h2 className={style.pages_anim_header}>Aplikacje webowe</h2>
                             </div>
                         </div>
-                        <p className={style.pages_desc}>Aplikacje webowe są dostosowane konkretnie do twoich potrzeb. Począwszy od prostych aplikacji ze stroną logowania i wykonujących podstawowe obliczenia a skończywszy na skomplikowanych i rozbudowanych z możliwością aktualicji za pomocą systemów CMS.</p>
-                        <p className={style.pages_desc}>Dzięki projektowaniu PWA (Progressive Web App) nie musisz płacić za wykonanie aplikacji mobilnej. Twoja aplikacja webowa będzie się zachowywać tak jak mobilna gdy bedzie używana na urządzeniu przenośnym. </p>
+                    </div>
+
+                    <div className={style.pages_desc}>
+                        <div className={style.pages_desc_content}>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Aplikacje webowe są dostosowane konkretnie do twoich potrzeb. Począwszy od prostych aplikacji ze stroną logowania a skończywszy na skomplikowanych z możliwością aktualicji za pomocą systemów CMS.</p>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <Preferences />
+                            </div>
+                        </div>
+                        <div className={style.pages_desc_content}>
+                            <div className={style.pages_desc_svg} data-aos="zoom-in">
+                                <Mobile />
+                            </div>
+                            <p data-aos="flip-up" className={style.pages_desc_P}>Dzięki projektowaniu PWA (Progressive Web App) nie musisz płacić za wykonanie aplikacji mobilnej. Twoja aplikacja webowa będzie się zachowywać tak jak mobilna gdy bedzie używana na urządzeniu przenośnym.</p>
+                        </div>
                     </div>
 
                     <h1 className={style.pages_h2}>Obejrzyj przykładowe elementy aplikacji webowych:</h1>
@@ -100,3 +158,5 @@ const Offer = () => {
 }
 
 export default Offer
+
+
