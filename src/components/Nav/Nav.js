5965190b4a85cf25ec55e0604a1a623c4192027d
Damian <wilczynskid@o2.pl>
2020-11-17 10:17:19 +0100

    archiwum

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -23,10 +23,10 @@ const Nav = props => {
                     <p className={style.headerDesc}>studio-www.com</p>
                 </div>
                 <ul onClick={() => setIsOpenMobileMenu(false)} className={`${style.list} ${styleMobileMenu}`}>
-                    <li className={style.listItem}><NavLink to='/home' activeClassName={style.activeLink} className={style.listItemAnchor}>STRONA GŁÓWNA</NavLink></li>
-                    <li className={style.listItem}><NavLink to='/offer' activeClassName={style.activeLink} className={style.listItemAnchor}>OFERTA</NavLink></li>
-                    <li className={style.listItem}><NavLink to='/login' activeClassName={style.activeLink} className={style.listItemAnchor}>STREFA KLIENTA</NavLink></li>
-                    <li className={style.listItem}><NavLink to='/contact' activeClassName={style.activeLink} className={style.listItemAnchor}>KONTAKT</NavLink></li>
+                    <li className={style.listItem}><NavLink to='/home' activeClassName={style.activeLink} className={style.listItemAnchor}>Strona główna</NavLink></li>
+                    <li className={style.listItem}><NavLink to='/offer' activeClassName={style.activeLink} className={style.listItemAnchor}>Oferta</NavLink></li>
+                    <li className={style.listItem}><NavLink to='/login' activeClassName={style.activeLink} className={style.listItemAnchor}>Strefa klienta</NavLink></li>
+                    <li className={style.listItem}><NavLink to='/contact' activeClassName={style.activeLink} className={style.listItemAnchor}>Kontakt</NavLink></li>
                 </ul>
                 <div onClick={() => setIsOpenMobileMenu(!isOpenMobileMenu)} className={`${style.burgerMenu} ${styleMobileButtonBurger}`}>
                     <div className={style.burgerBtn}></div>
