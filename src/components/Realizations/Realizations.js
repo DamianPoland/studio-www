482065675ff98769b81d0e0248566209bd1b11c0
oleleiNET <115529700+oleleiNET@users.noreply.github.com>
2023-04-12 16:47:40 +0200

    realizations update

--- src/components/Realizations/Realizations.js
+++ src/components/Realizations/Realizations.js
@@ -4,13 +4,13 @@ import AOS from 'aos'
 
 
 // images realizations
-import ketozocha from '../../assets/realizations/ketozocha.jpg'
-import beautystudio from '../../assets/realizations/beautystudio.jpg'
+// import ketozocha from '../../assets/realizations/ketozocha.jpg'
+// import beautystudio from '../../assets/realizations/beautystudio.jpg'
 import calendar from '../../assets/realizations/calendar.jpg'
 import getpol from '../../assets/realizations/getpol.jpg'
 import imgToText from '../../assets/realizations/imgToText.jpg'
 import jatestuje from '../../assets/realizations/jatestuje.jpg'
-import tatarczuk from '../../assets/realizations/tatarczuk.jpg'
+// import tatarczuk from '../../assets/realizations/tatarczuk.jpg'
 import wlaczremont from '../../assets/realizations/wlaczremont.jpg'
 
 
@@ -21,7 +21,7 @@ const Realizations = () => {
 
 
     // AOS reload after load first photo to not show animations before load screen
-    const [loadPhoto, setLoadPhoto] = useState(false)
+    const [loadPhoto,] = useState(false)
     useEffect(() => { AOS.refresh() }, [loadPhoto])
 
 
@@ -37,18 +37,18 @@ const Realizations = () => {
                     <h1 className="text2">Obejrzyj moje ostatnie realizacje:</h1>
                 </div>
 
-                <div className={style.realizations__item}>
+                {/* <div className={style.realizations__item}>
                     <div className={style.realizations__itemTextContainer}>
                         <h2 className="text1">Portale www</h2>
-                        <h3 className={style.realizations__text}>Blog dietetyczny: ketozocha.pl</h3>
-                        <a href="https://ketozocha.pl" target="_blank" rel="noopener noreferrer" className={style.realizations__link}>Zobacz...</a>
+                        <h3 className={style.realizations__text}>Blog dietetyczny: ketozocha.site</h3>
+                        <a href="https://ketozocha.site" target="_blank" rel="noopener noreferrer" className={style.realizations__link}>Zobacz...</a>
                     </div>
                     <a href="https://ketozocha.pl" target="_blank" rel="noopener noreferrer" data-aos="fade-up-left" className={style.realizations__figure}>
                         <img onLoad={() => setLoadPhoto(true)} className={style.img} src={ketozocha} alt='ketozocha.pl' />
                     </a>
-                </div>
+                </div> */}
 
-                <div className={style.realizations__item}>
+                {/* <div className={style.realizations__item}>
                     <div className={style.realizations__itemTextContainer}>
                         <h2 className="text1">Strony www</h2>
                         <h3 className={style.realizations__text}>Strona internetowa firmy hydraulicznej: tatarczuk.com.pl</h3>
@@ -59,7 +59,7 @@ const Realizations = () => {
                         <img className={style.img} src={tatarczuk} alt='tatarczuk.com.pl' />
                         <span className={`${style.animation__border} ${style.animation__borderRotate}`}></span>
                     </a>
-                </div>
+                </div> */}
 
                 <div className={style.realizations__item}>
                     <div className={style.realizations__itemTextContainer}>
@@ -111,7 +111,7 @@ const Realizations = () => {
                         </a>
                     </div>
 
-                    <div className={style.realizations__item}>
+                    {/* <div className={style.realizations__item}>
                         <div className={style.realizations__itemTextContainer}>
                             <h2 className="text1">Strony www</h2>
                             <h3 className={style.realizations__text}>Strona internetowa salonu piękności: beautystudio.com.pl</h3>
@@ -120,7 +120,7 @@ const Realizations = () => {
                         <a href="https://beautystudio.com.pl/" target="_blank" rel="noopener noreferrer" data-aos="fade-up-left" className={style.realizations__figure}>
                             <img className={style.img} src={beautystudio} alt='beautystudio.com.pl' />
                         </a>
-                    </div>
+                    </div> */}
 
                     <div className={style.realizations__item}>
                         <div className={style.realizations__itemTextContainer}>
